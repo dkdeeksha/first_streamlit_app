5421d6420595e7d439f8b4492fd73a062a66a1ed
Deeksha Koshta <87853282+dkdeeksha@users.noreply.github.com>
2023-05-31 14:02:34 +0530

    Update streamlit_app.py
    added breakfast menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Eggs')
